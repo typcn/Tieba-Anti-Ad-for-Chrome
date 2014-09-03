e03e726fb43275a2d38010d423d098e9de442ca8
typcn <admin@typcn.com>
2014-09-04 03:12:01 +0800

    更新去帖子红标题 / 修复帖子列表图片不显示

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
+function removeClass(obj, cls){
+		var obj_class = ' '+obj.className+' ';
+		obj_class = obj_class.replace(/(\s+)/gi, ' '),
+		removed = obj_class.replace(' '+cls+' ', ' ');
+		removed = removed.replace(/(^\s+)|(\s+$)/g, '');
+		obj.className = removed;
+}
 document.getElementsByClassName("sign_tip_sbox_hd")[0].innerHTML = "<a target='_blank' href='http://s.eqoe.cn'>自动签到</a>";
 var hyred = document.getElementsByClassName("threadlist_author");
 for (var i = 0; i < hyred.length; i++) {
@@ -7,14 +14,17 @@ var hyred2 = document.getElementsByClassName("d_author");
 for (var i = 0; i < hyred2.length; i++) {
 	hyred2[i].innerHTML = hyred2[i].innerHTML.replace("vip_red", "").replace("beMember_fl", "").replace(/style=.*no-repeat;border:none\"/g, "").replace(/src=.*?tb-lazyload=\"(.*?)\"/g, "src=\"$1\"")
 }
+var hyred3 = document.getElementsByClassName("threadlist_title");
+for (var i = 0; i < hyred3.length; i++) {
+	removeClass(hyred3[i],"member_thread_title_frs");
+}
 setTimeout(function() {
-	var hyred3 = document.getElementsByClassName("lzl_cnt");
-	for (var i = 0; i < hyred3.length; i++) {
-		hyred3[i].innerHTML = hyred3[i].innerHTML.replace("vip_red", "")
+	var hyred4 = document.getElementsByClassName("lzl_cnt");
+	for (var i = 0; i < hyred4.length; i++) {
+		hyred4[i].innerHTML = hyred4[i].innerHTML.replace("vip_red", "")
 	}
 },
 2000);
-document.getElementById("thread_list").innerHTML = document.getElementById("thread_list").innerHTML.replace(/fiesta_member/g, "");
 var levelnode = document.getElementsByClassName("user_level")[0].parentNode.parentNode;
 var infonode = document.getElementById("user_info").parentNode.parentNode;
 infonode.parentNode.insertBefore(levelnode, infonode.nextSibling);
